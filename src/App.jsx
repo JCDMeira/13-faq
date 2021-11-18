c031adb27509a587cf9e792398ac75db8c5c37af
Jean Meira <jean.meira10@hotmail.com>
2021-11-18 20:23:08 -0300

    Add: img and faq content

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { Conteiner, Content, GlobalStyle } from './global.js';
 import React from 'react';
 
+// # mobile images
+import mobileIllustration from './assets/images/img-mobile.svg';
+
 function App() {
   return (
     <>
       <GlobalStyle />
       <Conteiner>
         <Content>
-          <h1>FAQ</h1>
+          <div>
+            <img
+              src={mobileIllustration}
+              alt="illustration - Woman looking at a screen"
+              className="mobile-illustration"
+            />
+          </div>
+          <div className="faqContet">
+            <h1>FAQ</h1>
+          </div>
         </Content>
       </Conteiner>
     </>
--- src/global.js
+++ src/global.js
@@ -1,6 +1,25 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+:root{
+   // text - primary
+    --desaturated-blue: hsl(238, 29%, 16%);
+    --soft-red: hsl(14, 88%, 65%);
+
+    // text - neutral
+    --very-dark-grayish-blue: hsl(237, 12%, 33%);
+    --dark-grayish-blue:  hsl(240, 6%, 50%);
+
+    // background gradient
+    --soft-violet: hsl(273, 75%, 66%);
+    --soft-blue: hsl(240, 73%, 65%);
+    --darker-blue: hsl(260, 73%, 45%);
+    --white: #FFF;
+
+    // dividers
+    --light-grayish-blue: hsl(240, 5%, 91%);
+}
+
 *{
   padding: 0;
   margin: 0;
@@ -9,7 +28,7 @@ export const GlobalStyle = createGlobalStyle`
 
 html{
   font-size: 62.5%;
-  font-family: 'Kumbh Sans';
+  font-family: 'Kumbh Sans', sans-serif;
 }
 `;
 
@@ -29,19 +48,22 @@ export const Content = styled.div`
   background: #fff;
   box-shadow: 0px 12px 20px rgba(0, 0, 0, 0.25);
 
-  padding-top: 13rem;
-
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  h1 {
-    font-family: 'kumhb Sams';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 3rem;
-    line-height: 3.6rem;
+  .mobile-illustration {
+    margin-top: -11rem;
+    margin-bottom: 2.7rem;
+  }
 
-    color: #000000;
+  .faqContet {
+    h1 {
+      font-weight: 700;
+      font-size: 3rem;
+      line-height: 3.6rem;
+
+      color: #000000;
+    }
   }
 `;
